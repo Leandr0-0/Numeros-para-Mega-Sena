8727300daf3dfa0f159c54f01a6eb0d9af02c165
Leandro Silva <sr.syllw@gmail.com>
2023-03-09 09:41:42 -0300

    Acrescentando JS

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
-function intervaloDeNumeroAleatorio(a,b) {
+//function intervaloDeNumeroAleatorio(a,b) {
     return Math.floor(Math.random () * (b - a + 1)) + a
 }
 
-console.log(intervaloDeNumeroAleatorio(0, 10))
-console.log(intervaloDeNumeroAleatorio(555, 1111))
-console.log(intervaloDeNumeroAleatorio(89, 431))
+//console.log(intervaloDeNumeroAleatorio(0, 10))
+//console.log(intervaloDeNumeroAleatorio(555, 1111))
+//console.log(intervaloDeNumeroAleatorio(89, 431))
